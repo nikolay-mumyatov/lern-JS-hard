4a34483a07fad2b1119c05d43cb2cc024b394e7b
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-05-28 21:29:14 +0300

    Выполнение сложного задания lesson_02

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 "use strict";
 
+let num = 266219;
+
+console.log(num);
+
+let digits = num.toString().split('');
+
+let sum = digits[0] * digits[1] * digits[2] * digits[3] * digits[4] * digits[5];
+console.log(sum);
+
+let sumDegree = sum ** 3;
+console.log(sumDegree);
+
+console.log(sumDegree.toString().substring(0, 2));
