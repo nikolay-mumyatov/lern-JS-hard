3de486c04874404c17bc5da9e8aa187012681e90
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-05-31 19:17:50 +0300

    Сложное задание 3-его урока выполнил.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,43 @@
 "use strict";
 
+let lang = prompt('Введите значение языка "ru" или "en".');
+
+// if
+if (lang === 'ru') {
+  alert('Дни недели: Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else if (lang === 'en') {
+  alert(
+    'Weekday: Monday, Tuesday, Wednes­day, Thursday, Friday, Saturday, Sunday'
+  );
+} else {
+  alert('Вы ввели недопустимое значение! You entered an invalid value!');
+}
+
+// switch-case
+switch (lang) {
+  case 'ru':
+    alert(
+      'Дни недели: Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'
+    );
+  break;
+  case 'en':
+    alert(
+      'Weekday: Monday, Tuesday, Wednes­day, Thursday, Friday, Saturday, Sunday'
+    );
+  break;
+  default:
+   alert('Вы ввели недопустимое значение! You entered an invalid value!');
+}
+
+// многомерный массив
+let weekday = {
+  'ru': ['Понедельник', ' Вторник', ' Среда', ' Четверг', ' Пятница', ' Суббота', ' Воскресенье'],
+  'en': ['Monday', ' Tuesday', ' Wednes­day', ' Thursday', ' Friday', ' Saturday', ' Sunday'],
+};
+alert(weekday[lang]);
+
+// -------------------------------
+
+let namePerson = prompt('Введите свое имя.');
+namePerson.toLowerCase();
+console.log(namePerson === "артем" ? "Директор" : namePerson === "максим" ? "Преподаватель" : "Студент");
