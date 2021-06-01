13c0fcdf7c4c97e7652d3d7e82df38aecd63b483
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-06-02 00:26:44 +0300

    Выполнено сложное задание к уроку 04

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 "use strict";
 
+// Лучше input, потому, что prompt записывает только строку или null, но принцип и так понятен.
+let userValue = prompt("Введите строковое значение.");
+  
+let crop;
+const check = function (data) {
+  if (typeof data === "string" && data.length > 30) {
+    crop = data.trim().substr(0, 30) + "...";
+  } else if (typeof data === "string") {
+    crop = data.trim();
+  } else {
+    alert("Вы ввели не строковое значение!");
+  }
+};
+
+check(userValue);
+console.log(crop);
