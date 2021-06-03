8a42f7b9e02f56c655fd9d3f54dfb8c535be1487
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2021-06-03 17:43:01 +0300

    Выполнено сложное задание к 5 уроку.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 "use strict";
 
+// № 1
+let arr = [223, 324, 42, 987, 245, 40, 390];
+
+var arrLength = arr.length;
+for (var i = 0; i < arrLength; i++) {
+  let num = arr[i] + "";
+
+  if (num[0] === "2" || num[0] === "4") {
+    console.log(arr[i]);
+  }
+}
+
+// № 2
+
+console.log("----Простые числа");
+
+let number = 100;
+nextPrime: for (let i = 2; i <= number; i++) {
+
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      continue nextPrime;
+    }
+  }
+  console.log(i); // простое число
+}
